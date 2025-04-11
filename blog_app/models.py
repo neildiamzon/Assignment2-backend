3e5961db0896cc3811101f4b114170d19b56eeb0
admin <gazzellvillamante@gmail.com>
2025-04-11 20:56:52 +1200

    implement like and update user details functionality

--- blog_app/models.py
+++ blog_app/models.py
@@ -12,3 +12,19 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+    def like_count(self):
+        return self.likes.count()  # Count the number of likes related to this blog post
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    blog_post = models.ForeignKey(BlogPost, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'blog_post')  # Prevent a user from liking the same post multiple times
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.blog_post.title}"
+
--- blog_app/serializers.py
+++ blog_app/serializers.py
@@ -1,16 +1,19 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from blog_app.models import BlogPost
+from blog_app.models import BlogPost, Like
 
 
 class UserSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(max_length=30)
     last_name = serializers.CharField(max_length=30)
+    password = serializers.CharField(write_only=True, required=False)
+    username = serializers.CharField(required=False)
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
+        read_only_fields = ['id']
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
@@ -19,11 +22,27 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 class BlogPostSerializer(serializers.ModelSerializer):
+    like_count = serializers.IntegerField(read_only=True)  # Include like count
     image = serializers.ImageField(required=False)  # Make the image field optional for updates
 
     class Meta:
         model = BlogPost
-        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'image']
+        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'like_count', 'image']
         read_only_fields = ['id', 'author']  # author cannot be set from the request
 
+# Like serializer
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['user', 'blog_post', 'created_at']
+        read_only_fields = ['user', 'blog_post']
