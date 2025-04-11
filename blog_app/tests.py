c45aa3c0aaf3243227aadb7425ebb7fca3903a51
neil <neilaronndiamzon@gmail.com>
2025-04-11 23:56:58 +1200

    blog posts crud tests

--- blog_app/tests.py
+++ blog_app/tests.py
@@ -27,4 +27,47 @@ class BlogAppTestCases(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(BlogPost.objects.count(), 1)
+        print("Success blog post creation")
+
+    def test_delete_blog_post(self):
+        blog_post = BlogPost.objects.create(
+            title='Test Blog Post',
+            content='This is a test blog post.',
+            author=self.user
+        )
+
+        url = f'/api/blogposts/{blog_post.id}/delete/'
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(BlogPost.objects.count(), 0)
+        print("Success blog post deletion")
+
+    def test_update_blog_post(self):
+        blog_post = BlogPost.objects.create(
+            title='Test Blog Post',
+            content='This is a test blog post.',
+            author=self.user
+        )
+
+        url = f'/api/blogposts/{blog_post.id}/'
+        response = self.client.put(url, 
+                                   data={
+                                       'title': 'Updated Blog Post',
+                                       'content': 'This is an updated blog post.'
+                                   },
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print("Success blog post UPDATE")
+
+    def test_view_my_blog_post(self):
+        url = f'/api/blogposts/my/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print("Success view my blog posts")
+
+    def test_view_blog_posts(self):
+        url = f'/api/blogposts/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print("Success view all blog posts")
 
